99c1dcd2b25013b4f98256454a1275ed8a8d9764
Christhian-07 <118949897+Christhian-07@users.noreply.github.com>
2023-11-29 14:08:44 -0500

    s

--- PracticasParcial2/Practia P2-3/Backend/src/domain/dtos/index.ts
+++
@@ -1,9 +0,0 @@
-
-
-
-export * from './Category/create-category.dto';
-export * from './Category/update-category.dto';
-export * from './Service/create-service.dto';
-export * from './Service/update-service.dto';
-export * from './Product/create-product.dto';
-export * from './Product/update-product.dto';
